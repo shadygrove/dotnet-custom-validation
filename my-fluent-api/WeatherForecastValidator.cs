cd010849fbd68b179b63ec1643e3d869f4b3a38a
Martin Bour <mbour@topcon.com>
2020-06-16 15:49:36 -0400

    MB: Basic Fluent validation

---
+++ my-fluent-api/WeatherForecastValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_fluent_api
+{
+    public class WeatherForecastValidator : AbstractValidator<WeatherForecast>
+    {
+        public WeatherForecastValidator()
+        {
+            RuleFor(weather => weather.Summary).NotNull().NotEmpty();
+        }
+    }
+}
